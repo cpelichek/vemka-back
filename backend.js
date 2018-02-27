e62bfeee1df4a15c59d3f5a173b125e863e8b974
caio <cpelichek@gmail.com>
2018-02-27 18:35:06 -0300

    Made API post that pushes a new bought event into an array that belongs to a single user

--- backend.js
+++ backend.js
@@ -8,7 +8,7 @@ const events = require('events');
 
 const app = express();
 
-app.use(expressMongoDb('mongodb://localhost/8080'));
+app.use(expressMongoDb('mongodb://localhost/vemka-api'));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cors());
 
@@ -18,15 +18,17 @@ app.get('/schedules', (req, res) =>{
     req.db.collection('schedules')
     .find({})
     .toArray((err, data) =>{
+        console.log(data);
         res.send(data);
     })
 })
 
 //pega todos os perfis de usuário (user)
 app.get('/users', (req, res) =>{
-    req.deb.collection('users')
+    req.db.collection('users')
     .find({})
     .toArray((err, data) => {
+        console.log(data);
         res.send(data);
     })
 })
@@ -36,19 +38,20 @@ app.get('/userHistory/:id', (req, res) =>{
     let search ={
         _id: new ObjectID(req.params.id)
     };
-
+    
     req.db.collection('users')
     .findOne(search, (err, data) => {
         let scheduleIds = [];
         for(let schedule of data.scheduleBought){
             scheduleIds.push(new ObjectID(schedule._id));
         }
-
+        
         req.db.collection('schedules')
         .find({
             _id: { $in: scheduleIds }
         }).toArray ((err, scheduleArray) => {
-        res.send(scheduleArray);
+            console.log(data);
+            res.send(scheduleArray);
         })
     });
 });
@@ -57,34 +60,35 @@ app.get('/userHistory/:id', (req, res) =>{
 //manda informações para cadastrar novo usuário
 app.post('/signup', (req, res) => {
     console.log(req.body);
-
-    if (!req.body.firstName || req.body.lastName || req.body.phoneNumber || req.body.adress || req.body.cep || req.body.birthDate || req.body.email || req.body.password || req.body.agreementContract){
+    
+    if (!req.body.firstName || !req.body.lastName || !req.body.phoneNumber || !req.body.adress || !req.body.cep || !req.body.birthDate || !req.body.email || !req.body.password || !req.body.agreementContract){
         res.status(400).send({'error': 'Opa! Parece que você esqueceu de preencher algo!'});
         return;
     }
-
+    
     let user = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    username: `${firstName} ${lastName}`,
-    phoneNumber: req.body.phoneNumber,
-    adress: req.body.adress,
-    cep: req.body.cep,
-    birthDate: req.body.birthDate,
-    email: req.body.email,
-    password: req.body.password,
-    agreementContract: req.body.agreementContract,
-    scheduleBought: [{
-        scheduleId: "",
-        scheduleBoughtWith: "",
-        scheduleBoughtDate: "",
-    }]
-  }
-
-  req.db.collection('users')
-  .insert(user, (err, data) => {
-      res.send(data);
-  })
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        username: `${req.body.firstName} ${req.body.lastName}`,
+        phoneNumber: req.body.phoneNumber,
+        adress: req.body.adress,
+        cep: req.body.cep,
+        birthDate: req.body.birthDate,
+        email: req.body.email,
+        password: req.body.password,
+        agreementContract: req.body.agreementContract,
+        scheduleBought: [{
+            scheduleId: "",
+            scheduleBoughtWith: "",
+            scheduleBoughtDate: "",
+        }]
+    }
+    
+    req.db.collection('users')
+    .insert(user, (err, data) => {
+        console.log(data);
+        res.send(data);
+    })
 });
 
 //TODO
@@ -97,19 +101,19 @@ app.post('/signup', (req, res) => {
 //manda informações para cadastrar novo evento
 app.post('/newSchedule', (req, res) => {
     console.log(req.body);
-
+    
     //valida que os campos foram todos preenchidos
     if (!req.scheduleName || req.body.tracks || req.body.dateSchedule || req.body.dateSalesGoOnAir || req.body.dateSalesEnd || req.body.description || req.body.clue || req.body.locationName || req.body.locationAdress || req.body.boxItensList || req.body.speakerName || req.body.speakerEmail || req.body.speakerPhoneNumber){
         res.status(400).send({'error': 'Todos os campos são obrigatórios'});
         return;
     }
-
+    
     //cria a lista de itens da box
     let boxItens = [];
     for (item of req.body.boxItensList) { //boxItensList é o nome da variável array no front!
         boxItens.push({item});  //Está certo isso?
     }
-
+    
     let schedule = {
         scheduleName: req.body.name,
         tracks: req.body.tracks,
@@ -125,40 +129,81 @@ app.post('/newSchedule', (req, res) => {
         boxName: `Box ${scheduleName}`,
         boxItens: this.boxItens,
         speakers: [{
-          speakerName: req.body.speakerName,
-          speakerEmail: req.body.speakerEmail,
-          speakerPhoneNumber: req.body.speakerPhoneNumber
+            speakerName: req.body.speakerName,
+            speakerEmail: req.body.speakerEmail,
+            speakerPhoneNumber: req.body.speakerPhoneNumber
         }]
-      }
-
-  req.db.collection('schedules')
-  .insert(schedule, (err, data) => {
-      res.send(data);
-  })
+    }
+    
+    req.db.collection('schedules')
+    .insert(schedule, (err, data) => {
+        console.log(data);
+        res.send(data);
+    })
 });
 
 //TODO
 //manda informações para efetuar compra de um evento por um usuário
-app.post('/buy/:uid/:sid', (req, res) => {    //uid é _id do user & sid é _id do schedule
-    let userId ={
-        _id: new ObjectID(req.params.uid)
+app.post('/buy', (req, res) => {    //uid é _id do user & sid é _id do schedule
+    let userQuery ={
+        _id: new ObjectID(req.body.userId)
     }
 
-    let scheduleId ={
-        _id: new ObjectID(req.params.sid)
+    let scheduleQuery ={
+        _id: new ObjectID(req.body.scheduleId)
     }
-
+    
     let scheduleBoughtUpdate ={
-        scheduleId: this.scheduleId,
+        scheduleId: req.body.scheduleId,
         scheduleBoughtWith: req.body.scheduleBoughtWith,
         scheduleBoughtDate: Date.now()
     }
+    
+    req.db.collection('schedule')
+    .findOne(scheduleQuery, (err, schedule) => {
+        if(!schedule){
+            res.status(404).send({'error': 'Evento não encontrado'});
+            return;
+        }
 
-    req.db.collection('users')
-    .update({_id: {$in: userId}}, {$set: scheduleBought}, (err, data) => {
-        scheduleBought.push(scheduleBoughtUpdate);
-        res.send(data);
+        req.db.collection('users')
+        .findOne(userQuery, (err, user) => {
+            if(!user){
+                res.status(404).send({'error': 'Usuario não encontrado'});
+                return;
+            }
+
+            user.scheduleBought.push(scheduleBoughtUpdate);
+
+            req.db.collection('users').update(userQuery, user, (err) => {
+                res.send(user);
+            });
+        });
     });
+
+    req.db.collection('schedule')
+    .findOne(scheduleQuery, (err, schedule) => {
+        if(!schedule){
+            res.status(404).send({'error': 'Evento não encontrado'});
+            return;
+        }
+
+        req.db.collection('users')
+        .findOne(userQuery, (err, user) => {
+            if(!user){
+                res.status(404).send({'error': 'Usuario não encontrado'});
+                return;
+            }
+
+            user.scheduleBought.push(scheduleBoughtUpdate);
+
+            req.db.collection('users').update(userQuery, user, (err) => {
+                res.send(user);
+            });
+        });
+    });
+
+
 });
 
 //LISTENING
